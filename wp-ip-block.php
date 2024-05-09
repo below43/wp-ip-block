77d2a39a69b22b09689afc36baf6270a7b2f6482
Andrew Drake <andrew@below43.com>
2024-05-09 21:32:41 +1200

    Added toggle on/off setting and instructions about your own IP address

--- wp-ip-block.php
+++ wp-ip-block.php
@@ -25,20 +25,32 @@ function wp_ip_block_options() {
         update_option('wp_ip_block_addresses', $_POST['ip_addresses']);
     }
 
-    // Get the saved IP addresses
+
+    // Save the settings if the form has been submitted
+    if (isset($_POST['ip_addresses'], $_POST['enable_ip_block'])) {
+        update_option('wp_ip_block_addresses', $_POST['ip_addresses']);
+        update_option('wp_ip_block_enabled', $_POST['enable_ip_block']);
+    }
+
+    // Get the saved settings
     $ip_addresses = get_option('wp_ip_block_addresses', '');
+    $enable_ip_block = get_option('wp_ip_block_enabled', '');
 
     echo '<div class="wrap">';
     echo '<h2>WP IP Block</h2>';
     echo '<form method="post" action="">';
+    echo '<p><label for="enable_ip_block"><input type="checkbox" id="enable_ip_block" name="enable_ip_block" value="1"' . checked(1, $enable_ip_block, false) . '> Enable IP Block</label></p>';
     echo '<p><label for="ip_addresses">Allowed IP Addresses (one per line):</label></p>';
     echo '<p><textarea id="ip_addresses" name="ip_addresses" rows="10" cols="50">' . esc_textarea($ip_addresses) . '</textarea></p>';
+    echo '<p><strong>Warning:</strong> Make sure to add your current IP address to the list. Your current IP address is: ' . $_SERVER['REMOTE_ADDR'] . '</p>';
     echo '<p><input type="submit" value="Save Changes" class="button button-primary"></p>';
     echo '</form>';
+    echo '</div>';
+
 
     // Instructions for .htaccess IP block
     echo '<h3>.htaccess IP Block Instructions</h3>';
-    echo '<p>For extra security, to block all IP addresses except those listed above from accessing the wp-admin directory, add the following to your .htaccess file:</p>';
+    echo '<p>For extra security, to block all IP addresses except those listed above from accessing the wp-admin directory, add the following to your .htaccess file in wp-admin directory:</p>';
     echo '<pre>';
     echo "order deny,allow\n";
     echo "deny from all\n";
@@ -53,6 +65,11 @@ function wp_ip_block_options() {
 add_action('admin_init', 'wp_ip_block_check');
 
 function wp_ip_block_check() {
+    // Check if IP block is enabled
+    if (get_option('wp_ip_block_enabled', '') != 1) {
+        return;
+    }
+
 	// Get the saved IP addresses
 	$ip_addresses = get_option('wp_ip_block_addresses', '');
 
